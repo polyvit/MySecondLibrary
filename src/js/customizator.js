b44ed44733c10ee0d93563f18c5b68eeafcae2cb
Polina Limonova <polina98_08@mail.ru>
2023-03-29 14:38:15 +0300

    feat: save data in local storage

--- src/js/customizator.js
+++ src/js/customizator.js
@@ -2,16 +2,19 @@ export default class Customizator {
   constructor() {
     this.btnBlock = document.createElement("div");
     this.colorPicker = document.createElement("input");
+    this.clear = document.createElement("div");
+    this.scale = localStorage.getItem("scale") || 1;
+    this.color = localStorage.getItem("color") || "#ffffff";
     this.btnBlock.addEventListener("click", (e) => this.onScaleChange(e));
     this.colorPicker.addEventListener("input", (e) => this.onColorChange(e));
+    this.clear.addEventListener("click", () => this.reset());
   }
 
   onScaleChange(e) {
-    let scale;
     const body = document.querySelector("body");
-    if (e.target.value) {
-      scale = +e.target.value.replace(/x/g, "");
-      function recourse(el) {
+    if (e) {
+      this.scale = +e.target.value.replace(/x/g, "");
+      const recourse = (el) => {
         el.childNodes.forEach((node) => {
           if (
             node.nodeName === "#text" &&
@@ -27,32 +30,97 @@ export default class Customizator {
                 +value.replace(/px/g, "")
               );
               node.parentNode.style.fontSize =
-                node.parentNode.getAttribute("data-fz") * scale + "px";
+                node.parentNode.getAttribute("data-fz") * this.scale + "px";
             } else {
               node.parentNode.style.fontSize =
-                node.parentNode.getAttribute("data-fz") * scale + "px";
+                node.parentNode.getAttribute("data-fz") * this.scale + "px";
             }
           } else {
             recourse(node);
           }
         });
-      }
+      };
       recourse(body);
+
+      localStorage.setItem("scale", this.scale);
     }
   }
 
   onColorChange(e) {
     const body = document.querySelector("body");
     body.style.backgroundColor = e.target.value;
+    localStorage.setItem("color", e.target.value);
+  }
+
+  setBgColor() {
+    const body = document.querySelector("body");
+    body.style.backgroundColor = this.color;
+    this.colorPicker.value = this.color;
+  }
+
+  injectStyle() {
+    const style = document.createElement("style");
+    style.innerHTML = `
+      .panel {
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  position: fixed;
+  top: 10px;
+  right: 0;
+  border: 1px solid rgba(0, 0, 0, 0.2);
+  box-shadow: 0 0 20px rgba(0, 0, 0, 0.5);
+  width: 300px;
+  height: 60px;
+  background-color: #fff;
+}
+.scale {
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  width: 100px;
+  height: 40px;
+}
+.scale_btn {
+  display: block;
+  width: 40px;
+  height: 40px;
+  border: 1px solid rgba(0, 0, 0, 0.2);
+  border-radius: 4px;
+  font-size: 18px;
+  cursor: pointer;
+}
+.color {
+  width: 40px;
+  height: 40px;
+}
+.clear {
+  font-size: 20px;
+  cursor: pointer;
+}
+    `;
+    document.querySelector("head").appendChild(style);
+  }
+
+  reset() {
+    localStorage.clear();
+    this.scale = 1;
+    this.color = "#ffffff";
+    this.setBgColor();
+    this.onScaleChange();
   }
 
   render() {
+    this.injectStyle();
+    this.setBgColor();
+    this.onScaleChange();
     let scaleInputS = document.createElement("input"),
       scaleInputM = document.createElement("input"),
       panel = document.createElement("div");
 
-    panel.append(this.btnBlock, this.colorPicker);
-
+    panel.append(this.btnBlock, this.colorPicker, this.clear);
+    this.clear.innerHTML = "&times";
+    this.clear.classList.add("clear");
     scaleInputS.classList.add("scale_btn");
     scaleInputM.classList.add("scale_btn");
     this.btnBlock.classList.add("scale");
